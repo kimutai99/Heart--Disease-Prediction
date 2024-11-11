739bce4b4494c6902dc6c559b1e3431897aaff5b
brian kimutai <korosbrian574@gmail.com>
2024-11-11 23:21:15 +0300

    logger and Exception

---
+++ src/exception.py
@@ -0,0 +1,33 @@
+import sys
+from src.logger import logging
+
+def get_error_detail(error,error_detail:sys):
+    '''
+    This function generates error message that includes file name,line number and the original error message
+    '''
+    _, _, exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    line_number=exc_tb.tb_lineno
+    error_message=f'errror  occured in script:[{file_name}],line[{line_number}],message[{str(error)}]'
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error,error_detail:sys):
+        '''
+        Iniatialize the custom excption with the error message
+        '''
+        error_message=get_error_detail(error,error_detail)
+        
+        super().__init__(error_message)
+        self.error_message=error_message
+        
+    def __str__(self):
+        return  self.error_message     
+   
+   
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Divide by zero error')
+        raise CustomException(e,sys)      
---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from datetime import datetime
+
+log_dir=os.path.join(os.getcwd(),'logs')
+os.makedirs(log_dir,exist_ok=True)
+log_file=f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log'
+log_path=os.path.join(log_dir,log_file)
+logging.basicConfig(
+    filename=log_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
